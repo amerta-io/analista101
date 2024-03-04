5b0c00d25a4bf8efb69692aa52fe18079e4f99d3
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-04 23:16:14 +0700

    docs: update links and logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>analista101</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/amerta-io/analista101',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discordapp.com/users/589304761921306638',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+  docsRepositoryBase: 'https://github.com/amerta-io/analista101',
+  banner: {
+    text: 'This is a banner',
   },
 }
 
