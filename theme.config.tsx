885ab4de02d92eb17091e14fb71eacb057f45882
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-11 16:08:15 +0700

    docs: update page titles and links

--- theme.config.tsx
+++ theme.config.tsx
@@ -19,9 +19,6 @@ const config: DocsThemeConfig = {
     link: 'https://discordapp.com/users/589304761921306638',
   },
   docsRepositoryBase: 'https://github.com/amerta-io/analista101',
-  banner: {
-    text: 'This is a banner',
-  },
 }
 
 export default config
