9ba8008751b0e2f68b83300ff858969577f9c252
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-24 15:12:01 +0700

    Add babelIncludeRegex and modularizeImports to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  // babelIncludeRegex: {
-  //   '.*@mui/x-charts.*': true
-  // }
+  babelIncludeRegex: {
+    '.*@mui/x-charts.*': true
+  },
   transpilePackages: ['@mui/x-charts'],
-  // modularizeImports: {
-  //   "@mui/x-charts": {
-  //     transform: "@mui/x-charts/{{member}}",
-  //   }
-  // }
+  modularizeImports: {
+    "@mui/x-charts": {
+      transform: "@mui/x-charts/{{member}}",
+    }
+  }
 })
 
 module.exports = withNextra()
