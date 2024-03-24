3f6a45d4dbe7c368c0377a52d930d6c8b1c5a517
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-24 15:34:05 +0700

    Remove unnecessary code in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  babelIncludeRegex: {
-    '.*@mui/x-charts.*': true
-  },
   transpilePackages: ['@mui/x-charts'],
-  modularizeImports: {
-    "@mui/x-charts": {
-      transform: "@mui/x-charts/{{member}}",
-    }
-  }
 })
 
-module.exports = withNextra({
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.m?js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          },
-        },
-        include: /node_modules\/@mui\/x-charts/,
-      });
-    }
-    return config;
-  },
-})
+module.exports = withNextra()
